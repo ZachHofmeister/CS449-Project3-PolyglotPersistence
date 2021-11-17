2f5dfc9413a7cd62826c4e58d3ce8853bd09bcdb
ZachHofmeister <zachhof@me.com>
2021-11-17 10:27:30 +0000

    likePost() now increments the num of likes of a postin redis, and getPostLikes() returns that value.

--- likes.py
+++ likes.py
@@ -2,6 +2,8 @@
 import hug
 import redis
 
+# https://redis-py.readthedocs.io/en/stable/
+
 # http GET '192.168.1.68:5000/likes'
 
 @hug.get('/likes/')
@@ -15,30 +17,30 @@ def getAllLikes():
 # http GET '192.168.1.68:5000/likes/popular'
 
 @hug.get('/likes/popular/')
-def getPopularPosts():
+def getPopularPosts(response):
 	"""Returns list of posts with more than 1 like"""
 	#Unimplimented
 
 # http GET '192.168.1.68:5000/likes/user/zachattack'
 
 @hug.get('/likes/user/{username}')
-def getUserLikes():
+def getUserLikes(response, username):
 	"""Returns all the posts the user has liked"""
 	#Unimplimented
 
 # http GET '192.168.1.68:5000/likes/post/1'
 
 @hug.get('/likes/post/{postID}')
-def getPostLikes():
+def getPostLikes(response, postID):
 	"""Returns how many likes a post has received"""
-	#Unimplimented
+	postLikes = redis.Redis(host='localhost', port=6379, db=1)
+	return {"likes": postLikes.get(postID)}
 
 # http POST '192.168.1.68:5000/likes/post/1?username=willum'
 
 @hug.post('/likes/post/{postID}', status=hug.falcon.HTTP_201)
 def likePost(response, postID, username):
 	"""Add a like from username to the post"""
-	#Unimplimented
 	"""
 	When a user likes a post:
 		Add the post id to a list stored in the key of their username
@@ -47,6 +49,13 @@ def likePost(response, postID, username):
 			ex key-value:	postID: #likes
 		Update the number of likes for the post in the popular db
 	"""
+	#Should verify that username and postID exists, maybe also that user hasn't liked the post before
+
 	userLikes = redis.Redis(host='localhost', port=6379, db=0)
+
 	postLikes = redis.Redis(host='localhost', port=6379, db=1)
+	postLikes.incr(postID)
+
 	popular = redis.Redis(host='localhost', port=6379, db=2)
+
+
