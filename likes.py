00d5c4a050d567a2023f7bc46847ce1c79955bb3
ZachHofmeister <zachhof@me.com>
2021-11-17 10:09:25 +0000

    Started working on function to add likes

--- likes.py
+++ likes.py
@@ -7,7 +7,7 @@ import redis
 @hug.get('/likes/')
 def getAllLikes():
 	"""Returns all likes"""
-	#Unimplimented
+	#Unimplimented, not asked for by prof but might be helpful
 	r = redis.Redis(host='localhost', port=6379, db=0)
 	r.set('foo', 'bar')
 	return r.get('foo')
@@ -39,3 +39,14 @@ def getPostLikes():
 def likePost(response, postID, username):
 	"""Add a like from username to the post"""
 	#Unimplimented
+	"""
+	When a user likes a post:
+		Add the post id to a list stored in the key of their username
+			ex key-value:	username: [0, 12, 20]
+		Increment the number of likes stored in the key of the post ID
+			ex key-value:	postID: #likes
+		Update the number of likes for the post in the popular db
+	"""
+	userLikes = redis.Redis(host='localhost', port=6379, db=0)
+	postLikes = redis.Redis(host='localhost', port=6379, db=1)
+	popular = redis.Redis(host='localhost', port=6379, db=2)
