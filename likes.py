17d77695489b82b41d482aa082f70c63a3cbf01a
ZachHofmeister <zachhof@me.com>
2021-11-18 08:17:15 +0000

    Optimistically, finished likes service

--- likes.py
+++ likes.py
@@ -6,34 +6,46 @@ import redis
 
 # http GET '192.168.1.68:5000/likes'
 
-@hug.get('/likes/')
-def getAllLikes():
-	"""Returns all likes"""
-	#Unimplimented, not asked for by prof but might be helpful
-	r = redis.Redis(host='localhost', port=6379, db=0)
-	r.set('foo', 'bar')
-	return r.get('foo')
+# @hug.get('/likes/')
+# def getAllLikes():
+# 	"""Returns all likes"""
+# 	#Unimplimented, not asked for by prof but might be helpful
+# 	r = redis.Redis(host='localhost', port=6379, db=0)
+# 	r.set('foo', 'bar')
+# 	return r.get('foo')
+
+userDB = 0
+postDB = 1
+popularDB = 2
 
 # http GET '192.168.1.68:5000/likes/popular'
 
 @hug.get('/likes/popular/')
 def getPopularPosts(response):
-	"""Returns list of posts with more than 1 like"""
-	#Unimplimented
+	"""Returns list of top 10 posts"""
+	popular = redis.Redis(host='localhost', port=6379, db=popularDB)
+	postList = popular.zrevrange("popular", 0, 9, withscores=True)
+
+	#change key labels to be descriptive
+	for index, post in enumerate(postList):
+		postList[index] = {"post-id": post[0], "likes": post[1]}
+
+	return {"popular": postList}
 
 # http GET '192.168.1.68:5000/likes/user/zachattack'
 
 @hug.get('/likes/user/{username}')
 def getUserLikes(response, username):
 	"""Returns all the posts the user has liked"""
-	#Unimplimented
+	userLikes = redis.Redis(host='localhost', port=6379, db=userDB)
+	return {"posts": userLikes.smembers(username)}
 
 # http GET '192.168.1.68:5000/likes/post/1'
 
 @hug.get('/likes/post/{postID}')
 def getPostLikes(response, postID):
 	"""Returns how many likes a post has received"""
-	postLikes = redis.Redis(host='localhost', port=6379, db=1)
+	postLikes = redis.Redis(host='localhost', port=6379, db=postDB)
 	return {"likes": postLikes.get(postID)}
 
 # http POST '192.168.1.68:5000/likes/post/1?username=willum'
@@ -49,13 +61,19 @@ def likePost(response, postID, username):
 			ex key-value:	postID: #likes
 		Update the number of likes for the post in the popular db
 	"""
-	#Should verify that username and postID exists, maybe also that user hasn't liked the post before
+	#maybe TODO: Should verify that username and postID exists
 
-	userLikes = redis.Redis(host='localhost', port=6379, db=0)
+	userLikes = redis.Redis(host='localhost', port=6379, db=userDB)
+	if userLikes.sismember(username, postID):
+		response.status = hug.falcon.HTTP_409
+		return {"error": f"{username} has already liked post {postID}"}
+	
+	userLikes.sadd(username, postID)
 
-	postLikes = redis.Redis(host='localhost', port=6379, db=1)
-	postLikes.incr(postID)
+	postLikes = redis.Redis(host='localhost', port=6379, db=postDB)
+	postLikes.incrby(postID)
 
-	popular = redis.Redis(host='localhost', port=6379, db=2)
+	popular = redis.Redis(host='localhost', port=6379, db=popularDB)
+	popular.zincrby("popular", 1, postID)
 
 
